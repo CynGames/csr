b63ca8f83f1dbae362835f1c56f8902032e60dfa
CynGames <tomasm.leguizamon@gmail.com>
2022-11-17 12:09:53 -0300

    Responsive

--- src/components/3_Body/3_DonationSegment/DonationSegment.tsx
+++ src/components/3_Body/3_DonationSegment/DonationSegment.tsx
@@ -75,7 +75,6 @@ const DonationSegment = () => {
             <Grid
                 item
                 xs={12}
-                md={6}
             >
                 <Box
                     id={'donation'}
@@ -105,7 +104,6 @@ const DonationSegment = () => {
             <Grid
                 item
                 xs={12}
-                md={6}
             >
                 <Box m={2}>
 
@@ -119,52 +117,6 @@ const DonationSegment = () => {
                 </Box>
             </Grid>
 
-            {/*<Grid*/}
-            {/*    item*/}
-            {/*    xs={12}*/}
-            {/*    md={6}*/}
-            {/*>*/}
-            {/*    <Box*/}
-            {/*        id={'donation'}*/}
-            {/*        sx={{*/}
-            {/*            display: "flex",*/}
-            {/*            flexDirection: "column",*/}
-            {/*            alignItems: "center",*/}
-            {/*            m: 2*/}
-            {/*        }}>*/}
-            {/*        <Typography fontWeight={"bold"} variant={'h5'} fontFamily={"Helvetica Nue"} align="center">*/}
-            {/*            ¿Quieres asociarte?*/}
-            {/*            <br/>*/}
-            {/*            Necesitamos tu Ayuda*/}
-            {/*        </Typography>*/}
-            {/*        <hr/>*/}
-
-            {/*        <Typography variant={'h6'} fontFamily={"Helvetica Nue"} align="center" sx={{mb: 1}}>*/}
-            {/*            Rellená los datos debajo, tales como la entidad, el monto mensual y los meses que deseas*/}
-            {/*            contribuir.*/}
-            {/*            <br/>*/}
-            {/*            <br/>*/}
-            {/*            No te preocupes por moverte de tu casa, nosotros nos acercaremos a cobrarte y darte el recibo.*/}
-            {/*        </Typography>*/}
-            {/*        <hr/>*/}
-            {/*    </Box>*/}
-            {/*</Grid>*/}
-            {/*<Grid*/}
-            {/*    item*/}
-            {/*    xs={12}*/}
-            {/*    md={6}*/}
-            {/*>*/}
-            {/*    <Box m={2}>*/}
-
-            {/*        {NotifyOutcome(message)}*/}
-
-            {/*        <FormFieldHolder/>*/}
-            {/*        <Divider sx={{mb: 1.5, minWidth: 300}}/>*/}
-            {/*        <PaymentButtons/>*/}
-            {/*        <Divider sx={{mb: 1.5, minWidth: 300}}/>*/}
-            {/*        <DataSubmitter/>*/}
-            {/*    </Box>*/}
-            {/*</Grid>*/}
         </Grid>
 
     )
